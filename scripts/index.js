9740c28aa038f3abbdacbe3070eac9a998daac6c
Cydoniam <murad.cydonia@gmail.com>
2022-09-11 23:22:40 +0300

    More changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-const arrayHighRu = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ь','Ы','Ъ','Э','Ю','Я,'];
-const arrayLowRu = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ь','ы','ъ','э','ю','я,'];
+const arrayHighRu = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ь','Ы','Ъ','Э','Ю','Я'];
+const arrayLowRu = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ь','ы','ъ','э','ю','я'];
 const arrayHighEn = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const arrayLowEn = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 const symbols = [' ',',','.',':',';','!','?','-','_','=','+','(',')','[',']','@','`',"'",'"','<','>','|','/','%','$','^','&','*','~','1','2','3','4','5','6','7','8','9','0'];
@@ -24,7 +24,7 @@ const caesarCipher = (text, operation, shift) => {
 
         for (let i = 0; i < text.length; i++) {
             console.log(text[i])//test
-            for (let j = 0; j < arrayHighEn.length; j++) {
+            for (let j = 0; j < symbols.length; j++) {
                 if (text[i] === arrayHighEn[j]) {
                     console.log(text[i] === arrayHighEn[j])//test
                     out += text[i]
@@ -72,5 +72,5 @@ const caesarCipher = (text, operation, shift) => {
 
     console.log(out)//test
 
-    return out
+    return out, document.getElementById("result__text").innerHTML = out //what if
 }
