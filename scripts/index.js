eecd1d4fd7ee86b54c5757fb8848ac6d1ae419ee
Cydoniam <murad.cydonia@gmail.com>
2022-09-11 22:39:36 +0300

    Minor changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
-const arrayHighRu = ['АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ']
-const arrayLowRu = ['абвгдеёжзийклмнопрстуфхцчшщьыъэюя']
-const arrayHighEn = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
-const arrayLowEn = ['abcdefghijklmnopqrstuvwxyz']
+const arrayHighRu = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ь','Ы','Ъ','Э','Ю','Я,'];
+const arrayLowRu = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ь','ы','ъ','э','ю','я,'];
+const arrayHighEn = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const arrayLowEn = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const symbols = [' ',',','.',':',';','!','?','-','_','=','+','(',')','[',']','@','`',"'",'"','<','>','|','/','%','$','^','&','*','~','1','2','3','4','5','6','7','8','9','0'];
 
 const caesarButton = () => {
     if (document.getElementById("caesar__operation-choice").value === '1') {
@@ -22,11 +23,26 @@ const caesarCipher = (text, operation, shift) => {
     // if (operation === 1) {
         
         for (let i = 0; i < text.length; i++) {
-            for (let j = 0; j < arrayHighRu.length; j++) {
-                if (text[i] === arrayHighRu[j]) {
-                    out += text[i + shift] 
-                }
+            console.log(text[i])
+            for (let j = 0; j < arrayHighEn.length; j++) {
+                if (text[i] === arrayHighEn[j]) {
+                    console.log(text[i] === arrayHighEn[j])
+                    out += text[i]
+                } else if (text[i] === arrayHighRu[j]) {
+                    console.log(text[i] === arrayHighRu[j])
+                    out += text[i]
+                } else if (text[i] === arrayLowEn[j]) {
+                    console.log(text[i] === arrayLowEn[j])
+                    out += text[i]
+                } else if (text[i] === arrayLowRu[j]) {
+                    console.log(text[i] === arrayLowRu[j])
+                    out += text[i]
+                } else if (text[i] === symbols[j]) {
+                    console.log(text[i] === symbols[j])
+                    out += text[i]
+                } else {}
             }
+            
             // out += text.charCodeAt(i)
             // out += arrayHighEn[i].charCodeAt()
             // for(let j = 0; j < arrayHighRu.length;) {
