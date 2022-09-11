6f33b2eddba6c7cf2cec0ad0dde0b92fbfbcae69
Cydoniam <murad.cydonia@gmail.com>
2022-09-11 21:33:20 +0300

    Minor changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,7 @@
-const arrayHighRu = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ь','Ы','Ъ','Э','Ю','Я,'];
-const arrayLowRu = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ь','ы','ъ','э','ю','я,'];
-const arrayHighEn = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-const arrayLowEn = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-const symbols = [' ',',','.',':',';','!','?','-','_','=','+','(',')','[',']','@','`',"'",'"','<','>','|','/','%','$','^','&','*','~','1','2','3','4','5','6','7','8','9','0'];
+const arrayHighRu = ['АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ']
+const arrayLowRu = ['абвгдеёжзийклмнопрстуфхцчшщьыъэюя']
+const arrayHighEn = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+const arrayLowEn = ['abcdefghijklmnopqrstuvwxyz']
 
 const caesarButton = () => {
     if (document.getElementById("caesar__operation-choice").value === '1') {
@@ -13,17 +12,38 @@ const caesarButton = () => {
 }
 
 const caesarCipher = (text, operation, shift) => {
+
     let out = ""
-    text = document.getElementById('caesar__text-input').value.split('')
+
+    text = document.getElementById('caesar__text-input').value
     operation = document.getElementById('caesar__operation-choice').value
     shift = document.getElementById('caesar__shift-choice').value
 
-    // for(let i of text) {
-    //     out += String.fromCharCode(i.charCodeAt(0) + 1)
+    // if (operation === 1) {
+        
+        for (let i = 0; i < text.length; i++) {
+            for (let j = 0; j < arrayHighRu.length; j++) {
+                if (text[i] === arrayHighRu[j]) {
+                    out += text[i + shift] 
+                }
+            }
+            // out += text.charCodeAt(i)
+            // out += arrayHighEn[i].charCodeAt()
+            // for(let j = 0; j < arrayHighRu.length;) {
+            // }
+    
+
+            // str = (arrayHighEn[i - shift] === undefined) ? (arrayHighEn[i - shift - arrayHighEn.length]) : (arrayHighEn[i - shift])
+            // out = out.concat(str) 
+
+        }
+
+    // }
+    // if (operation === 2) {
+        
     // }
 
-    return out
-    // text.split('')
-}
+    console.log(out)
 
-// caesarCipher()
+    return out
+}
